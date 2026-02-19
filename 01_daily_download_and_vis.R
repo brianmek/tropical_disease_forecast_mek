4d235816d12387a8f4302cd82e28653432850c29
parasarker <117391560+parasarker@users.noreply.github.com>
2026-02-19 14:31:02 -0500

    Edited packages installed and loaded for data download
    Added installation of the 'readr' package, removed unneccesary inclusion of 'httr' package

--- 01_daily_download_and_vis.R
+++ 01_daily_download_and_vis.R
@@ -3,8 +3,8 @@
 # Corresponds to 2/20/2026 "Pulling and Visualizing Data" Project Milestone
 
 #### part 1: load packages ####
+install.packages(readr)
 library(readr)
-library(httr)
 
 #### part 2: bring in data ####
 disease_url = 'https://minio-s3.apps.shift.nerc.mghpcc.org/bu4cast-ci-read/challenges/targets/project_id=bu4cast/tropical-disease-targets.csv'
